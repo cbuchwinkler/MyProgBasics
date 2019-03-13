7bc96344f58c3910b99f9081677efa422347a1ab
cbuchwinkler <43604671+cbuchwinkler@users.noreply.github.com>
2019-03-13 17:43:59 +0100

    update

---
+++ Assets/MyProject/Scripts/ProgBasicsPartOne.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgBasicsPartOne : MonoBehaviour {
+
+   private int index = 0;
+   private int step = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CountUp(3);
+        Text("myStringText");
+        MultNumbers(3.3f, 1.0f);
+        MixTypes("firstTry", 4, 3.9f);
+
+    }
+
+    private void CountUp()
+    {
+        index = index + step;
+        Debug.Log(index);
+    }
+
+    private void CountUp(int mystep)
+    {
+        index = index + mystep;
+        Debug.Log(index);
+    }
+
+    private void Text (string text)
+    {
+        Debug.Log(text);
+    }
+
+    private void MultNumbers(float first, float second)
+    {
+        float tmp = first * second;
+        Debug.Log("MultNumbers:" + tmp);
+    }
+
+    private void MixTypes(string test, int index, float end)
+    {
+        Debug.Log(test + " , " + index + " , " + end);
+    }
+}
